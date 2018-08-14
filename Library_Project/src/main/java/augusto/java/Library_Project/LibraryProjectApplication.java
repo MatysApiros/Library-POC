1f03f35d145f298f0ba59793c67f4f290b9aa117
AugustoKlaic <augusto.klaic@acad.pucrs.br>
2018-08-14 14:07:38 -0300

    Commit inicial.

---
+++ Library_Project/src/main/java/augusto/java/Library_Project/LibraryProjectApplication.java
@@ -0,0 +1,12 @@
+package augusto.java.Library_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+}
