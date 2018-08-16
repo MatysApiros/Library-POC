8d8f2afd3f144a060c5d657deb0f5da54ae7d141
AugustoKlaic <augusto.klaic@acad.pucrs.br>
2018-08-16 15:43:52 -0300

    Primeiro service de listar todos os livros.

--- Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/service/BookService.java
+++ Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/service/BookService.java
@@ -1,5 +1,15 @@
 package augusto.java.Library_Project.service;
 
+import augusto.java.Library_Project.dto.Book;
+import augusto.java.Library_Project.repository.BookRepository;
+
+import java.util.List;
+
 public class BookService {
 
+    private BookRepository bookRepository;
+
+    public List<Book> addBook(){
+        return bookRepository.listAllBooks();
+    }
 }
