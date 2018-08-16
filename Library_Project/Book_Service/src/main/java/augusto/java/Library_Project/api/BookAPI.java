04bec2368bd5172985da1119cf1776a1689eb08e
AugustoKlaic <augusto.klaic@acad.pucrs.br>
2018-08-16 13:58:47 -0300

    Esqueleto da API.

--- Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/api/BookAPI.java
+++ Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/api/BookAPI.java
@@ -3,9 +3,7 @@ package augusto.java.Library_Project.api;
 import augusto.java.Library_Project.dto.Book;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +15,23 @@ public class BookAPI {
 
 
 
-    @GetMapping
-    public void getBook(){
+    @GetMapping("/list")
+    public void listAllBooks(){
+
     }
 
+    @DeleteMapping("/remove/{title}")
+    public void deleteBook(){
+
+    }
+
+    @PostMapping("/insert")
+    public void addBook(){
+
+    }
+
+    @GetMapping("/find/{title}")
+    public void findBook(){
+
+    }
 }
