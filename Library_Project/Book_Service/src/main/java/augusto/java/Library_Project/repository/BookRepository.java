b9d0143ad4caa75361a2871317a56a79dfa5e889
gutos <augusto.klaic@acad.pucrs.br>
2018-08-20 20:50:55 -0300

    Correcao BookRepository.

--- Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/repository/BookRepository.java
+++ Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/repository/BookRepository.java
@@ -2,11 +2,8 @@ package augusto.java.Library_Project.repository;
 
 import augusto.java.Library_Project.dto.Book;
 import com.mongodb.client.MongoCollection;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.bson.Document;
 import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +13,6 @@ public class BookRepository {
 
     private MongoCollection<Document> mongoCollection;
 
-    @Autowired
     public BookRepository(MongoCollection<Document> mongoCollection){
         this.mongoCollection = mongoCollection;
     }
@@ -28,7 +24,4 @@ public class BookRepository {
                 .map(books -> new Book(books.getInteger("id"), books.getString("title"), books.getString("author"), books.getString("genre")))
                 .collect(Collectors.toList());
     }
-
-
-    
 }
