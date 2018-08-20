819c183f0577f68b9d8291485be580b8e1fabac3
gutos <augusto.klaic@acad.pucrs.br>
2018-08-20 20:53:13 -0300

    Alteracao BookService.

--- Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/service/BookService.java
+++ Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/service/BookService.java
@@ -2,14 +2,21 @@ package augusto.java.Library_Project.service;
 
 import augusto.java.Library_Project.dto.Book;
 import augusto.java.Library_Project.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BookService {
 
     private BookRepository bookRepository;
 
-    public List<Book> addBook(){
+    public BookService(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> ListAllBooks(){
         return bookRepository.listAllBooks();
     }
 }
