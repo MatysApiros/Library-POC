f959fff5093a47197b4a187500fe565a8b146aae
gutos <augusto.klaic@acad.pucrs.br>
2018-08-20 20:47:17 -0300

    Modificacao API, primeira funcao.

--- Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/api/BookAPI.java
+++ Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/api/BookAPI.java
@@ -1,23 +1,29 @@
 package augusto.java.Library_Project.api;
 
-import augusto.java.Library_Project.dto.Book;
+import augusto.java.Library_Project.output.BookOutput;
+import augusto.java.Library_Project.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@Controller
 @RestController
 @RequestMapping("/books")
 public class BookAPI {
 
+    private BookService bookService;
+    private ObjectMapper objectMapper;
+
+    public BookAPI(BookService bookService, ObjectMapper objectMapper){
+        this.bookService = bookService;
+        this.objectMapper = objectMapper;
+    }
 
 
     @GetMapping("/list")
-    public void listAllBooks(){
-
+    public ResponseEntity<?> listAllBooks(){
+        return ResponseEntity.ok(bookService.ListAllBooks()
+                .stream().map(book -> objectMapper.convertValue(book, BookOutput.class)));
     }
 
     @DeleteMapping("/remove/{title}")
