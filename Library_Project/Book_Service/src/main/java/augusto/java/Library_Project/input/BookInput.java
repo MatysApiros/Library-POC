fdd601cf120d344bab3f57deab74ec9a471454ee
gutos <augusto.klaic@acad.pucrs.br>
2018-08-22 19:59:54 -0300

    Adicao BookInput.

---
+++ Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/input/BookInput.java
@@ -0,0 +1,50 @@
+package augusto.java.Library_Project.input;
+
+import javax.validation.constraints.NotEmpty;
+
+public class BookInput {
+
+    @NotEmpty
+    private int id;
+
+    @NotEmpty
+    private String title;
+
+    @NotEmpty
+    private String author;
+
+    @NotEmpty
+    private String genre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
