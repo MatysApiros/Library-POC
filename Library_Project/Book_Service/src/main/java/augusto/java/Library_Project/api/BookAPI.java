07b908a6ab4788f105b09aacdf48efd65466a934
AugustoKlaic <augusto.klaic@acad.pucrs.br>
2018-09-04 15:48:01 -0300

    Alteracao da API para receber objetos via JSON

--- Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/api/BookAPI.java
+++ Library_Project/Book_Service/src/main/java/augusto/java/Library_Project/api/BookAPI.java
@@ -1,5 +1,6 @@
 package augusto.java.Library_Project.api;
 
+import augusto.java.Library_Project.dto.Book;
 import augusto.java.Library_Project.input.BookInput;
 import augusto.java.Library_Project.mapper.BookMapper;
 import augusto.java.Library_Project.output.BookOutput;
@@ -26,23 +27,18 @@ public class BookAPI {
                 .stream().map(book -> objectMapper.convertValue(book, BookOutput.class)));
     }
 
-    @RequestMapping("/remove")
-    public ResponseEntity<?> deleteBook(@RequestParam("title") String title) {
-        return ResponseEntity.ok(bookService.deleteBook(title));
+    @DeleteMapping("/remove")
+    public ResponseEntity<?> deleteBook(@RequestBody BookInput bookInput) {
+        return ResponseEntity.ok(bookService.deleteBook(bookInput.getTitle()));
     }
 
-    @RequestMapping("/insert")
-    public ResponseEntity<?> addBook(@RequestParam("title") String title, @RequestParam("author") String author, @RequestParam("genre") String genre) {
-        BookInput bookInput = new BookInput();
-        bookInput.setTitle(title);
-        bookInput.setAuthor(author);
-        bookInput.setGenre(genre);
-        bookService.insertBook(BookMapper.map(bookInput));
-        return ResponseEntity.ok("Anexado.");
+    @PostMapping("/insert")
+    public ResponseEntity<?> addBook(@RequestBody BookInput bookInput) {
+        return ResponseEntity.ok(bookService.insertBook(BookMapper.map(bookInput)));
     }
 
-    @GetMapping("/find")
-    public ResponseEntity<?> findBook(@RequestParam("title") String title) {
+    @GetMapping("/find/{title}")
+    public ResponseEntity<?> findBook(@PathVariable("title") String title) {
         return ResponseEntity.ok(objectMapper.convertValue(bookService.findBookByTitle(title), BookOutput.class));
     }
 }
